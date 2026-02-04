2acbe84c067cd76a3632a6fc6c42544801a67922
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-02-04 10:47:48 -0500

    Updates to sparse_lu

--- sp_lu/driver.py
+++ sp_lu/driver.py
@@ -9,6 +9,8 @@
 # imports
 import time
 import numpy as np
+from scipy import sparse
+from scipy.sparse import coo_matrix
 from scipy.sparse import csc_matrix
 from scipy.sparse import linalg as la
 import matplotlib.pyplot as plt
@@ -36,57 +38,45 @@ def makeplots(D):
 
     # figure 1: original structure and L,U factors
     lu = la.splu(D, permc_spec='NATURAL')
-    LU = lu.L+lu.U
+    LU = lu.L + lu.U
     fig, axarr = plt.subplots(1,2)
     axarr[0].spy(D)
-    axarr[0].set_title('Original matrix (nnz = ' + str(D.getnnz()) + ')')
-    axarr[1].spy(LU.A)
-    axarr[1].set_title('Original: L+U (nnz = ' + str(LU.getnnz()) + ')')
+    axarr[0].set_title('Original matrix (nnz = ' + f"{D.getnnz():,}" + ')')
+    axarr[1].spy(LU)
+    axarr[1].set_title('Original: L+U (nnz = ' + f"{LU.nnz:,}" + ')')
 
 
     # figure 2: approximate minimum degree on D.T@D
     lu = la.splu(D, permc_spec='MMD_ATA')
-    LU = lu.L+lu.U
-    Pr = csc_matrix((n,n))
-    Pr[lu.perm_r, np.arange(n)] = 1
-    Pc = csc_matrix((n,n))
-    Pc[np.arange(n), lu.perm_c] = 1
-    PD = Pr * D * Pc
+    LU = lu.L + lu.U
+    PD = csc_matrix(lu.L @ lu.U)
     fig, axarr = plt.subplots(1,2)
-    axarr[0].spy(PD.A)
-    axarr[0].set_title('Approximate minimum degree on A.T @ A (nnz = ' + str(PD.getnnz()) + ')')
-    axarr[1].spy(LU.A)
-    axarr[1].set_title('AMD A.T @ A: L+U (nnz = ' + str(LU.getnnz()) + ')')
+    axarr[0].spy(PD)
+    axarr[0].set_title('Approximate minimum degree on A.T @ A (nnz = ' + f"{PD.nnz:,}" + ')')
+    axarr[1].spy(LU)
+    axarr[1].set_title('AMD A.T @ A: L+U (nnz = ' + f"{LU.nnz:,}" + ')')
 
 
     # figure 3: approximate minimum degree on A.T+A
     lu = la.splu(D, permc_spec='MMD_AT_PLUS_A')
-    LU = lu.L+lu.U
-    Pr = csc_matrix((n,n))
-    Pr[lu.perm_r, np.arange(n)] = 1
-    Pc = csc_matrix((n,n))
-    Pc[np.arange(n), lu.perm_c] = 1
-    PD = Pr * D * Pc
+    LU = lu.L + lu.U
+    PD = csc_matrix(lu.L @ lu.U)
     fig, axarr = plt.subplots(1,2)
-    axarr[0].spy(PD.A)
-    axarr[0].set_title('Approximate minimum degree on A.T + A (nnz = ' + str(PD.getnnz()) + ')')
-    axarr[1].spy(LU.A)
-    axarr[1].set_title('AMD A.T + A: L+U (nnz = ' + str(LU.getnnz()) + ')')
+    axarr[0].spy(PD)
+    axarr[0].set_title('Approximate minimum degree on A.T + A (nnz = ' + f"{PD.nnz:,}" + ')')
+    axarr[1].spy(LU)
+    axarr[1].set_title('AMD A.T + A: L+U (nnz = ' + f"{LU.nnz:,}" + ')')
 
 
     # figure 4: approximate minimum degree on column ordering
-    fig, axarr = plt.subplots(1,2)
     lu = la.splu(D, permc_spec='COLAMD')
-    LU = lu.L+lu.U
-    Pr = csc_matrix((n,n))
-    Pr[lu.perm_r, np.arange(n)] = 1
-    Pc = csc_matrix((n,n))
-    Pc[np.arange(n), lu.perm_c] = 1
-    PD = (Pr * D * Pc)
-    axarr[0].spy(PD.A)
-    axarr[0].set_title('Approximate minimum degree column order (nnz = ' + str(PD.getnnz()) + ')')
-    axarr[1].spy(LU.A)
-    axarr[1].set_title('COLAMD: L+U (nnz = ' + str(LU.getnnz()) + ')')
+    LU = lu.L + lu.U
+    PD = csc_matrix(lu.L @ lu.U)
+    fig, axarr = plt.subplots(1,2)
+    axarr[0].spy(PD)
+    axarr[0].set_title('Approximate minimum degree column order (nnz = ' + f"{PD.nnz:,}" + ')')
+    axarr[1].spy(LU)
+    axarr[1].set_title('COLAMD: L+U (nnz = ' + f"{LU.nnz:,}" + ')')
 
     plt.show()
 
@@ -118,8 +108,11 @@ def diff_2D(Nx,Ny):
     def ij(i,j):
         return(j*Nx + i)
 
-    # initialize the output matrix
-    D = np.zeros((Nx*Ny,Nx*Ny), dtype=float)
+    # initialize the data and row/column index arrays
+    nnzmax = Nx*Ny*5
+    rows = np.zeros(nnzmax, dtype=float)
+    cols = np.zeros(nnzmax, dtype=float)
+    vals = np.zeros(nnzmax, dtype=float)
 
     # set differencing constants
     dx = 1.0/(Nx-1)
@@ -129,23 +122,47 @@ def diff_2D(Nx,Ny):
     Diag = 1.0 + 2.0*(Dx2i + Dy2i)
 
     # iterate over the domain
+    idx = 0
     for iy in range(Ny):
         for ix in range(Nx):
 
             # set the matrix entries for this row of D
-            D[ ij(ix,iy), ij(ix,iy) ] = Diag
+            #   diagonal
+            rows[idx] = ij(ix,iy)
+            cols[idx] = ij(ix,iy)
+            vals[idx] = Diag
+            idx += 1
+
+            #   x-left
             if (ix > 0):
-                D[ ij(ix,iy), ij(ix-1,iy) ] = -Dx2i
+                rows[idx] = ij(ix,iy)
+                cols[idx] = ij(ix-1,iy)
+                vals[idx] = -Dx2i
+                idx += 1
+
+            #   x-right
             if (ix < Nx-1):
-                D[ ij(ix,iy), ij(ix+1,iy) ] = -Dx2i
+                rows[idx] = ij(ix,iy)
+                cols[idx] = ij(ix+1,iy)
+                vals[idx] = -Dx2i
+                idx += 1
+
+            #   y-left
             if (iy > 0):
-                D[ ij(ix,iy), ij(ix,iy-1) ] = -Dy2i
+                rows[idx] = ij(ix,iy)
+                cols[idx] = ij(ix,iy-1)
+                vals[idx] = -Dy2i
+                idx += 1
+
+            #   y-right
             if (iy < Ny-1):
-                D[ ij(ix,iy), ij(ix,iy+1) ] = -Dy2i
+                rows[idx] = ij(ix,iy)
+                cols[idx] = ij(ix,iy+1)
+                vals[idx] = -Dy2i
+                idx += 1
 
-    Dcsc = csc_matrix(D)
+    Dcsc = csc_matrix(coo_matrix((vals, (rows,cols)), shape=(Nx*Ny, Nx*Ny)))
     return Dcsc
-
 
 def diff_3D(Nx,Ny,Nz):
     r"""
@@ -174,8 +191,11 @@ def diff_3D(Nx,Ny,Nz):
     def ijk(i,j,k):
         return (k*Nx*Ny + j*Nx + i)
 
-    # initialize the output matrix
-    D = np.zeros((Nx*Ny*Nz,Nx*Ny*Nz), dtype=float)
+    # initialize the data and row/column index arrays
+    nnzmax = Nx*Ny*Nz*7
+    rows = np.zeros(nnzmax, dtype=float)
+    cols = np.zeros(nnzmax, dtype=float)
+    vals = np.zeros(nnzmax, dtype=float)
 
     # set differencing constants
     dx = 1.0/(Nx-1)
@@ -187,26 +207,63 @@ def diff_3D(Nx,Ny,Nz):
     Diag = 1.0 + 2.0*(Dx2i + Dy2i + Dz2i)
 
     # iterate over the domain
+    idx = 0
     for iz in range(Nz):
         for iy in range(Ny):
             for ix in range(Nx):
 
                 # set the matrix entries for this row of D
-                D[ ijk(ix,iy,iz), ijk(ix,iy,iz) ] = Diag
-                if (ix > 0):
-                    D[ ijk(ix,iy,iz), ijk(ix-1,iy,iz) ] = -Dx2i
-                if (ix < Nx-1):
-                    D[ ijk(ix,iy,iz), ijk(ix+1,iy,iz) ] = -Dx2i
-                if (iy > 0):
-                    D[ ijk(ix,iy,iz), ijk(ix,iy-1,iz) ] = -Dy2i
-                if (iy < Ny-1):
-                    D[ ijk(ix,iy,iz), ijk(ix,iy+1,iz) ] = -Dy2i
-                if (iz > 0):
-                    D[ ijk(ix,iy,iz), ijk(ix,iy,iz-1) ] = -Dz2i
-                if (iz < Nz-1):
-                    D[ ijk(ix,iy,iz), ijk(ix,iy,iz+1) ] = -Dz2i
 
-    Dcsc = csc_matrix(D)
+                #   diagonal
+                rows[idx] = ijk(ix,iy,iz)
+                cols[idx] = ijk(ix,iy,iz)
+                vals[idx] = Diag
+                idx += 1
+
+                #   x-left
+                if (ix > 0):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix-1,iy,iz)
+                    vals[idx] = -Dx2i
+                    idx += 1
+
+                #   x-right
+                if (ix < Nx-1):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix+1,iy,iz)
+                    vals[idx] = -Dx2i
+                    idx += 1
+
+                #   y-left
+                if (iy > 0):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix,iy-1,iz)
+                    vals[idx] = -Dy2i
+                    idx += 1
+
+                #   y-right
+                if (iy < Ny-1):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix,iy+1,iz)
+                    vals[idx] = -Dy2i
+                    idx += 1
+
+                #   z-left
+                if (iz > 0):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix,iy,iz-1)
+                    vals[idx] = -Dz2i
+                    idx += 1
+
+                #   z-right
+                if (iz < Nz-1):
+                    rows[idx] = ijk(ix,iy,iz)
+                    cols[idx] = ijk(ix,iy,iz+1)
+                    vals[idx] = -Dz2i
+                    idx += 1
+
+
+    Dcsc = csc_matrix(coo_matrix((vals, (rows,cols)), shape=(Nx*Ny*Nz, Nx*Ny*Nz)))
     return Dcsc
 
 
@@ -220,19 +277,16 @@ if __name__ == "__main__":
     print("problem 1: small 2D diffusion matrix")
     D = diff_2D(5,10)
     makeplots(D)
-    input("Press enter to continue")
 
     # problem 2: larger 2D diffusion matrix
     print("problem 2: larger 2D diffusion matrix");
     D = diff_2D(50,100)
     makeplots(D)
-    input("Press enter to continue")
 
     # problem 3: small 3D diffusion matrix
     print("problem 3: small 3D diffusion matrix");
     D = diff_3D(5,8,10)
     makeplots(D)
-    input("Press enter to continue")
 
     # problem 4: larger 3D diffusion matrix
     print("problem 4: larger 3D diffusion matrix");
